30fa7fde24ceec5aa73d3da3097edafdfb96abc2
GBTC440014UR <GBTC440014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-14 20:30:56 -0400

    https://github.com/javaiggy/ex-3_03.git

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController
+{
+    @Autowired
+    TaskRepository taskRepository;
+
+    @RequestMapping("/")
+    public String listToDoItems(Model model)
+    {
+        model.addAttribute("tasks", taskRepository.findAll() );
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String toDoForm(Model model)
+    {
+        model.addAttribute("task", new Task()  );
+        return "todoform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Task task, BindingResult result)
+    {
+        if (result.hasErrors() )
+        {
+            return "todoform";
+        }
+
+        taskRepository.save(task);
+        return "redirect:/";
+
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showTask(@PathVariable("id") long id, Model model)
+    {
+        model.addAttribute("task", taskRepository.findById(id).get() );
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateTask(@PathVariable("id")  long id,  Model model)
+    {
+        model.addAttribute("task", taskRepository.findById(id).get() );
+        return "todoform";
+    }
+
+
+    @RequestMapping("/delete/{id}")
+    public String delTask(@PathVariable("id") long id)
+    {
+        taskRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+}
+
